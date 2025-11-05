a5b028041dd43851600242c09cddacc39c29aca6
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-05 13:46:36 -0500

    init

--- .devcontainer/scripts/install_dockercli.sh
+++ .devcontainer/scripts/install_dockercli.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+apt-get update && apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    gnupg
+
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
@@ -8,8 +13,5 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
 | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-apt-get update && apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    gnupg \
+apt-get install -y \
     docker-ce-cli
