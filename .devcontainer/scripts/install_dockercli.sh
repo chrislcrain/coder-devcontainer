bfbf0aae95211449323c8d0a298caedc13e830d2
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-05 13:49:08 -0500

    init

--- .devcontainer/scripts/install_dockercli.sh
+++ .devcontainer/scripts/install_dockercli.sh
@@ -14,5 +14,5 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
 | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-apt-get install -y \
+apt-get update && apt-get install -y \
     docker-ce-cli
