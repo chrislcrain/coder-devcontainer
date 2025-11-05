6b73a5bb9dc740fe7f658e57e13afdfd0f2df3fc
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-05 13:45:45 -0500

    init

--- .devcontainer/scripts/install_dockercli.sh
+++ .devcontainer/scripts/install_dockercli.sh
@@ -9,4 +9,7 @@ https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_C
 | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 apt-get update && apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    gnupg \
     docker-ce-cli
