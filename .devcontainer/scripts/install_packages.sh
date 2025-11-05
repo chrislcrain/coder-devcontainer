af134f2b7a842cd987c8ea94ac49652c0cfbb29a
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-05 12:00:39 -0500

    init

--- .devcontainer/scripts/install_packages.sh
+++ .devcontainer/scripts/install_packages.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+| tee /etc/apt/sources.list.d/docker.list >/dev/null
+
 apt-get update && apt-get install -y \
     pipx \
     gh \
@@ -35,4 +40,5 @@ apt-get update && apt-get install -y \
     libxmlsec1-dev \
     libffi-dev \
     liblzma-dev \
+    docker-ce-cli \
  && rm -rf /var/lib/apt/lists/*
