b21946dd9f9240be1f4a00236652b2433398eb8f
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-07 14:11:27 -0500

    init

--- .devcontainer/scripts/configure_zsh.sh
+++ .devcontainer/scripts/configure_zsh.sh
@@ -1,39 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -d /home/coder ]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/coder/.powerlevel10k
-  # Add to .zshrc if not present
-  grep -qxF 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' "/home/coder/.zshrc" || \
-    echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >> "/home/coder/.zshrc"
-fi
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Define variables
 USER_HOME="/home/coder"
 ZSH_CUSTOM="$USER_HOME/.oh-my-zsh/custom"
 ZSHRC="$USER_HOME/.zshrc"
 SYNTAX_HIGHLIGHTING="$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 AUTOSUGGESTIONS="$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
-# Ensure Oh My Zsh exists
-if [ ! -d "$USER_HOME/.oh-my-zsh" ]; then
-  echo "Oh My Zsh not found in $USER_HOME/.oh-my-zsh. Aborting."
-  exit 1
-fi
+# Install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/coder/.powerlevel10k
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install zsh-syntax-highlighting
-if [ ! -d "$SYNTAX_HIGHLIGHTING" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$SYNTAX_HIGHLIGHTING"
-fi
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$SYNTAX_HIGHLIGHTING"
 
 # Install zsh-autosuggestions
-if [ ! -d "$AUTOSUGGESTIONS" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions "$AUTOSUGGESTIONS"
-fi
-
-# Set correct ownership
-chown coder:coder "$ZSHRC" "$SYNTAX_HIGHLIGHTING" "$AUTOSUGGESTIONS" -R
+git clone https://github.com/zsh-users/zsh-autosuggestions "$AUTOSUGGESTIONS"
 
 echo "Plugins installed and .zshrc updated for user coder."
