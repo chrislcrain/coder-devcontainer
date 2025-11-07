b3a081797738ac9278efecdf205e01e1811439a5
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-07 13:24:58 -0500

    init

--- .devcontainer/scripts/install_poetry.sh
+++ .devcontainer/scripts/install_poetry.sh
@@ -5,5 +5,5 @@ pipx ensurepath
 
 ZSH_CUSTOM="/home/coder/.oh-my-zsh"
 
-mkdir $ZSH_CUSTOM/plugins/poetry
+mkdir -p $ZSH_CUSTOM/plugins/poetry
 poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
