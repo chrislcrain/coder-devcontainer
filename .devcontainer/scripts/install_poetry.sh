95727b0a7cba55ca92d67f2609a2bd4a4ee9d6a1
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-07 13:26:39 -0500

    init

--- .devcontainer/scripts/install_poetry.sh
+++ .devcontainer/scripts/install_poetry.sh
@@ -7,3 +7,13 @@ ZSH_CUSTOM="/home/coder/.oh-my-zsh"
 
 mkdir -p $ZSH_CUSTOM/plugins/poetry
 poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
+
+plugin="zsh-autosuggestions"
+zshrc="$HOME/.zshrc"
+
+# Add the plugin if it's not already present
+if ! grep -qE "plugins=.*\b$plugin\b" "$zshrc"; then
+  # Insert the plugin into the plugins() array (at the end)
+  sed -i'' -e "s/^\(plugins=.*\)(/\1 $plugin)/" "$zshrc"
+fi
+
