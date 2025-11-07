066f6ba73fae470de01fc1c8e0e977630fa2975d
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-07 10:59:19 -0500

    init

---
+++ .devcontainer/scripts/install_poetry.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pipx install poetry==1.8.2
+pipx ensurepath
--- .devcontainer/scripts/install_pyenv.sh
+++ .devcontainer/scripts/install_pyenv.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 curl -fsSL https://pyenv.run | bash
 
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init - zsh)"' >> ~/.zshrc
+grep -qxF 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+grep -qxF '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+grep -qxF 'eval "$(pyenv init - zsh)"' ~/.zshrc || echo 'eval "$(pyenv init - zsh)"' >> ~/.zshrc
