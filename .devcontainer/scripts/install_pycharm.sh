dc77a89fe674f0238e4b9576cc42eda138aa02de
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-11 16:14:18 -0500

    create plinks for db

--- .devcontainer/scripts/install_pycharm.sh
+++ .devcontainer/scripts/install_pycharm.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-wget https://download.jetbrains.com/idea/code-with-me/backend/jetbrains-clients-downloader-linux-x86_64-1867.tar.gz && \
-tar -xzvf jetbrains-clients-downloader-linux-x86_64-1867.tar.gz
-rm jetbrains-clients-downloader-linux-x86_64-1867.tar.gz
+TARGET_DIR="/home/coder"
 
-./jetbrains-clients-downloader-linux-x86_64-1867/bin/jetbrains-clients-downloader --products-filter PY --build-filter 252.26199.168 --platforms-filter linux-x64 --download-backends ~/JetBrains
-tar -xzvf ~/JetBrains/backends/PY/*.tar.gz -C ~/JetBrains/backends/PY
-rm -rf ~/JetBrains/backends/PY/*.tar.gz
+wget -O "$TARGET_DIR/jetbrains-clients-downloader-linux-x86_64-1867.tar.gz" \
+  https://download.jetbrains.com/idea/code-with-me/backend/jetbrains-clients-downloader-linux-x86_64-1867.tar.gz
+
+tar -xzvf "$TARGET_DIR/jetbrains-clients-downloader-linux-x86_64-1867.tar.gz" -C "$TARGET_DIR"
+rm "$TARGET_DIR/jetbrains-clients-downloader-linux-x86_64-1867.tar.gz"
+
+"$TARGET_DIR/jetbrains-clients-downloader-linux-x86_64-1867/bin/jetbrains-clients-downloader" \
+  --products-filter PY \
+  --build-filter 252.26199.168 \
+  --platforms-filter linux-x64 \
+  --download-backends "$TARGET_DIR/JetBrains"
+
+tar -xzvf "$TARGET_DIR"/JetBrains/backends/PY/*.tar.gz -C "$TARGET_DIR/JetBrains/backends/PY"
+rm -rf "$TARGET_DIR"/JetBrains/backends/PY/*.tar.gz
