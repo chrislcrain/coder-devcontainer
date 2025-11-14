6eb77d6e75ca4861c2ee67ebb6ef39c3964592ec
chriscrain <chriscrain@Chriss-MacBook-Pro.local>
2025-11-14 12:25:18 -0500

    init

--- .devcontainer/scripts/set_root_user.sh
+++ .devcontainer/scripts/set_root_user.sh
@@ -2,7 +2,8 @@
 
 if [ "${USER_IS_ROOT}" = "true" ]; then
   usermod -aG sudo coder
-  echo 'coder ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+  echo 'coder ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/coder
+  chmod 440 /etc/sudoers.d/coder
   echo "User set as root."
 else
   echo "User is not set as root: skipping sudoer setup."
