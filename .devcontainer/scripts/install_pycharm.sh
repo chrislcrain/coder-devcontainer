66b37ad6a5c304d550145b738cdafce75eb2cf49
chriscrain <22624146+chrislcrain@users.noreply.github.com>
2025-11-18 22:20:30 -0500

    create sram infra team

--- .devcontainer/scripts/install_pycharm.sh
+++ .devcontainer/scripts/install_pycharm.sh
@@ -9,6 +9,12 @@ curl -fL \
 tar -xzvf "$TARGET_DIR/jetbrains-clients-downloader-linux-x86_64-1867.tar.gz" -C "$TARGET_DIR"
 rm "$TARGET_DIR/jetbrains-clients-downloader-linux-x86_64-1867.tar.gz"
 
+mkdir -p "$TARGET_DIR/JetBrains/backends/PY"
+
+curl -fL \
+  -o "$TARGET_DIR/JetBrains/backends/PY/pycharm-2025.2.4.tar.gz" \
+  "https://download.jetbrains.com/python/pycharm-2025.2.4.tar.gz"
+
 "$TARGET_DIR/jetbrains-clients-downloader-linux-x86_64-1867/bin/jetbrains-clients-downloader" \
   --products-filter PY \
   --build-filter 252.27397.106 \
